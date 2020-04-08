82e6afd03423f2c77cb5b0e197c2d1221f5aaef4
binhk1004 <binhk1004@naver.com>
2020-04-08 22:49:34 +0900

    Personal Project
    1. 지도 api 불러오기 실패
    
    2. 문의 남겨 놓음
    
    2020. 04. 08

--- app.py
+++ app.py
@@ -4,10 +4,6 @@ from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
-client = MongoClient('localhost', 27017)
-
-
-
 # HTML을 주는 부분
 @app.route('/')
 def index():
@@ -17,25 +13,11 @@ def index():
 def main():
     return render_template('main.html')
 
-
-# API 역할을 하는 부분
-@app.route('/', methods=['POST'])
-def play_map():
-
-    return jsonify({'result': 'success','stars_list':stars})
-
-
-@app.route('/api/like', methods=['POST'])
-def star_like():
-
-    return jsonify({'result': 'success'})
-
-
-@app.route('/api/delete', methods=['POST'])
-def star_delete():
-
-    return jsonify({'result': 'success'})
-
+@app.route('/main.html', methods=['POST', 'GET'])
+def result():
+    if request.method == 'GET':
+        result = request.form
+        return render_template('main.html', prediction = result)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
