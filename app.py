20c02161cd57455ad98b26480b26246825b0d460
binhk1004 <binhk1004@naver.com>
2020-04-09 20:32:45 +0900

    Personal Project
    1. 지도 api 불러오기 성공
    
    2. Mongo DB 데이터 삽입 성공
    
    3. DB 불러오기 실패
    
    2020. 04. 09

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from pymongo import MongoClient
-
 from flask import Flask, render_template, jsonify, request
-
 app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.Project_DB
 
 # HTML을 주는 부분
 @app.route('/')
@@ -13,11 +14,11 @@ def index():
 def main():
     return render_template('main.html')
 
-@app.route('/main.html', methods=['POST', 'GET'])
+@app.route('/main.html', methods=['GET'])
 def result():
     if request.method == 'GET':
-        result = request.form
-        return render_template('main.html', prediction = result)
+        return jsonify({'result':'success', 'msg': '데이터베이스 접속 성공'})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
--- static/main.js
+++ static/main.js
@@ -1,18 +1,62 @@
+window.onload = function () {
+  var mapContainer = document.getElementById("map"), // 지도를 표시할 div
+    mapOption = {
+      center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+      level: 7, // 지도의 확대 레벨
+    };
+
+  // 지도를 생성합니다
+  var map = new kakao.maps.Map(mapContainer, mapOption);
+
+  // 주소-좌표 변환 객체를 생성합니다
+  var geocoder = new kakao.maps.services.Geocoder();
+
+  // 주소로 좌표를 검색합니다
+  geocoder.addressSearch(juso, function (result, status) {
+    // 정상적으로 검색이 완료됐으면
+    if (status === kakao.maps.services.Status.OK) {
+      var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+      // 결과값으로 받은 위치를 마커로 표시합니다
+      var marker = new kakao.maps.Marker({
+        map: map,
+        position: coords,
+      });
+
+      // 인포윈도우로 장소에 대한 설명을 표시합니다
+      var infowindow = new kakao.maps.InfoWindow({
+        content:
+          '<div style="width:150px;text-align:center;padding:6px 0;">우리회사</div>',
+      });
+      infowindow.open(map);
+
+      // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+      map.setCenter(coords);
+    }
+  });
+};
+
 temp = location.href.split("?");
 data = temp[1].split("=");
 search_result = data[1];
-let result = decodeURIComponent(search_result);
-console.log(result);
+let juso = decodeURIComponent(search_result);
+console.log(juso);
 
 $(document).ready(function () {
-  $("#test").append(result);
+  $("#test").append(juso);
 });
 
-var container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
-var options = {
-  //지도를 생성할 때 필요한 기본 옵션
-  center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-  level: 3, //지도의 레벨(확대, 축소 정도)
-};
-
-var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+$.ajax({
+  type: "GET",
+  url: "/main.html",
+  data: {
+    address: decodeURIComponent(search_result),
+  },
+  success: function (response) {
+    if (response["result"] == "success") {
+      alert("데이터베이스 접속 성공");
+    } else {
+      alert("실패");
+    }
+  },
+});
