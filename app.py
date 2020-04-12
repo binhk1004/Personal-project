7dbe3927f17aa36be0bcf9a060ad2266e64aa3d2
binhk1004 <binhk1004@naver.com>
2020-04-12 21:06:41 +0900

    Personal Project
    1. 데이터 불러오기 성공
    
    2. 불러온 데이터 백엔드에서 확인 완료
    
    3. 개별공시지가 api 불러오기 성공
    
    4. 그래프 예시 생성 완료
    
    5. 개별공시지가 2015~2019년 데이터 불러오기 성공
    
    2020. 04. 12

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, jsonify, request
+import requests
+from urllib.parse import unquote
+
 app = Flask(__name__)
 
 from pymongo import MongoClient
@@ -10,23 +13,38 @@ db = client.Project_DB
 def index():
     return render_template('index.html')
 
-@app.route('/main.html')
+@app.route('/main')
 def main():
     return render_template('main.html')
 
-@app.route('/main.html', methods=['POST'])
+@app.route('/main', methods=['POST'])
 def find_list():
     NB_receive = request.form['local_NB']
-    local_result = list(db.Local_DB.find({'법정동명':"local_NB"}, {'_id': False}))
-    return jsonify({'result':'success', 'msg': '데이터베이스 접속 성공'})
+    local_result = list(db.Local_DB.find({'법정동명':NB_receive}, {'_id': False}))
+    local_num = local_result[0]['법정동코드']
+    print(local_num)
 
-# @app.route('/main.html', methods=['GET'])
-# def show_list():
-#     # 1. mystar 목록 전체를 검색합니다. ID는 제외하고 like 가 많은 순으로 정렬합니다.
-#     # 참고) find({},{'_id':False}), sort()를 활용하면 굿!
-#     DB = list(db.Local_DB.find({},{'_id':False}))
-#     # 2. 성공하면 success 메시지와 함께 stars_list 목록을 클라이언트에 전달합니다.
-#     return jsonify({'result': 'success','show_list':DB})
+    result_list = []
+
+    for i in range(2015, 2020):
+        result_list.append(change_year(i,local_num))
+
+    print(result_list)
+    return jsonify({'result':'success', 'msg': result_list})
+
+
+def change_year(year,local_num):
+    params = {
+        'ServiceKey': unquote(
+            'jFJq%2B%2BJgU2Mc8PrE5BxRZetsGiBrM%2BXDSku%2FUFCuzZ7j8FrslWnJ%2BR2xa7QbRStVG9HfSDU%2BmBQz3SCSfZmfXw%3D%3D'),
+        'ldCode': local_num, 'stdrYear': year, 'format': 'json', 'numOfRows': '10', 'pageNo': '1'
+    }
+    result = requests.get('http://apis.data.go.kr/1611000/nsdi/ReferLandPriceService/attr/getReferLandPriceAttr', params=params)
+
+    return result.json()
+
+
+
 
 
 if __name__ == '__main__':
---
+++ static/main.js
@@ -0,0 +1,77 @@
+$(document).ready(function () {
+  let juso = getJuso();
+  drawMap(juso);
+  find_result(juso);
+});
+
+function getJuso() {
+  let temp = location.href.split("?");
+  let data = temp[1].split("=");
+  let search_result = data[1];
+  return decodeURIComponent(search_result).replace(/\+/g, " ");
+}
+
+function drawMap(juso) {
+  let mapContainer = document.getElementById("map"), // 지도를 표시할 div
+    mapOption = {
+      center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+      level: 7, // 지도의 확대 레벨
+    };
+
+  // 지도를 생성합니다
+  let map = new kakao.maps.Map(mapContainer, mapOption);
+
+  // 주소-좌표 변환 객체를 생성합니다
+  let geocoder = new kakao.maps.services.Geocoder();
+
+  // 주소로 좌표를 검색합니다
+  geocoder.addressSearch(juso, function (result, status) {
+    // 정상적으로 검색이 완료됐으면
+    if (status === kakao.maps.services.Status.OK) {
+      let coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+      // 결과값으로 받은 위치를 마커로 표시합니다
+      let marker = new kakao.maps.Marker({
+        map: map,
+        position: coords,
+      });
+
+      // 인포윈도우로 장소에 대한 설명을 표시합니다
+      let infowindow = new kakao.maps.InfoWindow({
+        content:
+          '<div style="width:150px;text-align:center;padding:6px 0;">우리회사</div>',
+      });
+      infowindow.open(map);
+
+      // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+      map.setCenter(coords);
+    }
+  });
+}
+
+function find_result(keyword) {
+  $.ajax({
+    type: "POST",
+    url: "/main",
+    data: { local_NB: keyword },
+    success: function (response) {
+      if (response["result"] == "success") {
+        alert(response["msg"]);
+      } else {
+        alert(keyword);
+      }
+    },
+  });
+}
+
+var hello = [5, 10, 15, 20, 50, 60, 95, 80, 60];
+
+d3.select("body")
+  .select(".mid")
+  .data(hello)
+  .enter()
+  .append("div")
+  .attr("class", "chart")
+  .style("height", function (d) {
+    return d * 5 + "px";
+  });
